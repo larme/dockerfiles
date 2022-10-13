68d9b8313e56afe09c0be698ffb09b47b2a4a1e9
Zhao Shenyang <dev@zsy.im>
2022-10-14 05:08:26 +0800

    add cpu-dev image

---
+++ cpu-dev/run.sh
@@ -0,0 +1,14 @@
+docker run --name cpu-devbox -h cpu-devbox -it --rm \
+       -v ~/codes:/workspace/codes \
+       -v ~/bentoml:/workspace/bentoml \
+       -v ~/notebooks:/workspace/notebooks \
+       -v ~/soft:/workspace/soft \
+       -v ~/j:/workspace/j \
+       -v ~/x:/workspace/x \
+       -v ~/tmp:/workspace/tmp \
+       -v ~/.local/dockerspace/cpu-devbox:/workspace/.local \
+       -v ~/.ssh:/home/larme/.ssh \
+       -v /var/run/docker.sock:/var/run/docker.sock \
+       -e HOST_USER_ID=$(id -u $USER) \
+       -e HOST_GROUP_ID=$(id -g $USER) \
+       larme/cpu-dev:latest
